afe7d7520fef42a955ed5082dfa826d156896767
FelineJTD <13520050@std.stei.itb.ac.id>
2024-02-27 08:55:18 +0100

    feat: first occurence

--- first-occurence.py
+++ first-occurence.py
@@ -1,27 +1,18 @@
 # imports
 import sys
-import math
 
 filename = sys.argv[1]
 search = sys.argv[2]
+
 # read file
 with open(filename, 'r') as f:
     text = f.read()
 
-frequency = {}
-cur = 0
-arr_of_steps = []
 for i in range(0, len(text)):
     try:
         if (text[i] == sys.argv[2][0] and text[i+1] == sys.argv[2][1] and text[i+2] == sys.argv[2][2]):
-            if cur != 0:
-                arr_of_steps.append(i-cur)
-            cur = i
+            print('First occurence at index: ', i)
+            print('Column: ', i%12)
+            break
     except:
         pass
-
-print('Array of steps: ')
-print(arr_of_steps)
-
-print('Possible key length (by GCD): ')
-print(math.gcd(*arr_of_steps))
--- frequency-analysis-step.py
+++ frequency-analysis-step.py
@@ -2,6 +2,7 @@
 import sys
 
 filename = sys.argv[1]
+step = int(sys.argv[2])
 # read file
 with open(filename, 'r') as f:
     text = f.read()
@@ -10,8 +11,6 @@ with open(filename, 'r') as f:
 frequency = {
     'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0, 'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0
 }
-
-step = 3
 
 # loop through text and count occurences
 for i in range(0, len(text), step):
