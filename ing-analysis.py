2eff5819c16c07517756fbcc5834020ad086d370
FelineJTD <13520050@std.stei.itb.ac.id>
2024-02-27 10:49:05 +0700

    wip: add playfair rules

---
+++ ing-analysis.py
@@ -0,0 +1,25 @@
+# imports
+import sys
+
+filename = sys.argv[1]
+# read file
+with open(filename, 'r') as f:
+    text = f.read()
+
+frequency = {}
+freq_one = {}
+for i in range(0, len(text)):
+    if text[i] == 'Q' and text[i+1] == 'X':
+        try:
+            bigram = text[i+2] + text[i+3]
+            try:
+                frequency[bigram] += 1
+                freq_one[text[i+3]] += 1
+            except:
+                frequency[bigram] = 1
+                freq_one[text[i+3]] = 1
+        except:
+            pass
+
+print(dict(sorted(frequency.items(), key=lambda item: item[1], reverse=True)))
+print(dict(sorted(freq_one.items(), key=lambda item: item[1], reverse=True)))
--- playfair-part-decrypt.py
+++ playfair-part-decrypt.py
@@ -10,7 +10,6 @@ with open(filename, 'r') as f:
 plaintext = ''
 
 substitution = {}
-# THELW
 substitution['HE'] = 'TH'
 substitution['EH'] = 'HT'
 substitution['EL'] = 'HE'
@@ -19,10 +18,15 @@ substitution['HL'] = 'TE'
 substitution['LH'] = 'ET'
 substitution['QX'] = 'IN'
 substitution['XQ'] = 'NI'
+substitution['IN'] = 'QX'
 substitution['MH'] = 'RE'
 substitution['HM'] = 'ER'
 substitution['TM'] = 'EV'
 substitution['MT'] = 'VE'
+substitution['LV'] = 'T_'
+substitution['VL'] = '_T'
+substitution['LM'] = 'E_'
+substitution['ML'] = '_E'
 substitution['KH'] = 'YW'
 substitution['HK'] = 'WY'
 substitution['LR'] = 'H_'
@@ -38,6 +42,18 @@ substitution['EB'] = 'T_'
 # substitution['LD'] = 'AT'
 # substitution['MH'] = 'NT'
 
+more_subs = {}
+
+# propagate substitutions
+for key in substitution:
+    # balikan
+    more_subs[key[1] + key[0]] = substitution[key][1] + substitution[key][0]
+    # square
+    if (key[0] != key[1]) and (key[1] != substitution[key][0]) and (substitution[key][0] != substitution[key][1]) and (substitution[key][0] != '_') and (substitution[key][1] != '_'):
+        more_subs[substitution[key][0] + substitution[key][1]] = key[0] + key[1]
+        more_subs[substitution[key][1] + substitution[key][0]] = key[1] + key[0]
+
+
 for i in range(0, len(text), 2):
     try:
         bigram = text[i] + text[i+1]
@@ -47,6 +63,8 @@ for i in range(0, len(text), 2):
             plaintext += '__'
     except:
         pass
+
+print(plaintext)
 
 linewidth = 100
 with open('output/playfair-subs.txt', 'w') as f:
