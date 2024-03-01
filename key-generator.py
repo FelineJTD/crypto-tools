6b133c3f9781533c74aad579fec170024768e048
FelineJTD <13520050@std.stei.itb.ac.id>
2024-03-01 08:22:31 +0100

    checkpoint: playfair

---
+++ key-generator.py
@@ -0,0 +1,21 @@
+# THELA
+# VRMUS
+
+key_matrix = [['T', 'H', 'E', 'L', 'A'], ['V', 'R', 'M', '_', 'S']]
+
+print('Possible subsitution keys: ')
+
+# row
+for h in range(0, 2):
+  for i in range(0, 5):
+    for j in range(i+1, 5):
+      print(f'substitution[\'{key_matrix[h][(i)%5]}{key_matrix[h][(j)%5]}\'] = \'{key_matrix[h][(i+4)%5]}{key_matrix[h][(j+4)%5]}\'')
+      print(f'substitution[\'{key_matrix[h][(j)%5]}{key_matrix[h][(i)%5]}\'] = \'{key_matrix[h][(j+4)%5]}{key_matrix[h][(i+4)%5]}\'')
+
+# box
+for i in range(0, 5):
+  for j in range(i+1, 5):
+    print(f'substitution[\'{key_matrix[0][i%5]}{key_matrix[1][j%5]}\'] = \'{key_matrix[0][j%5]}{key_matrix[1][i%5]}\'')
+    print(f'substitution[\'{key_matrix[1][i%5]}{key_matrix[0][j%5]}\'] = \'{key_matrix[1][j%5]}{key_matrix[0][i%5]}\'')
+    print(f'substitution[\'{key_matrix[0][j%5]}{key_matrix[1][i%5]}\'] = \'{key_matrix[0][i%5]}{key_matrix[1][j%5]}\'')
+    print(f'substitution[\'{key_matrix[1][j%5]}{key_matrix[0][i%5]}\'] = \'{key_matrix[1][i%5]}{key_matrix[0][j%5]}\'')
